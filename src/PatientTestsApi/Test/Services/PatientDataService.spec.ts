02d7310b681aa71e1f0b38925cf7d60c57bd0cba
Daniel Rocha <daroch@microsoft.com>
2020-05-15 16:06:27 +0800

    Added integration tests

--- src/PatientTestsApi/Test/Services/PatientDataService.spec.ts
+++ src/PatientTestsApi/Test/Services/PatientDataService.spec.ts
@@ -25,6 +25,30 @@ describe("PatientDataService #integration", async function (): Promise<void> {
     expect(createdPatient._shardKey).is.equal(id);
   }); 
 
+  it("Won't find a patient that does not exist", async function (): Promise<void> {
+    const dataService: PatientDataService = createPatientDataService();
+    const id = '-123';
+
+    // test
+    const createdPatient = await dataService.findPatient(id);
+    expect(createdPatient).to.be.null;
+  }); 
+
+  it("Can find a patient", async function (): Promise<void> {
+    const dataService: PatientDataService = createPatientDataService();
+    const expectedPatient = PatientFixture.createPatient();
+    expectedPatient.id = 'newId';    
+    const id = await dataService.insertPatient(expectedPatient);
+
+    // test
+    const createdPatient = await dataService.findPatient(id);
+    Object.keys(expectedPatient).forEach(key => {
+      expect(createdPatient[key]).deep.equal(expectedPatient[key]);
+    });
+    expect(createdPatient._id).is.equal(id);
+    expect(createdPatient._shardKey).is.equal(id);
+  }); 
+
   after(async function (): Promise<void> {
     await db.cleanPatients();
     await db.close();
